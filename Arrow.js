4a7dfc3785c20aa3df90ef0ef5aba1adb1706400
ArlifTagaree <liffism9@gmail.com>
2023-03-17 13:43:28 +0700

    Arrow

---
+++ Arrow.js
@@ -0,0 +1,14 @@
+//Arrow function
+
+
+// function fullName(fNamne,lName) {
+    
+//     return fNamne+lName;
+// }
+
+// console.log(fullName('Arlif','Tagreee'));
+
+fullName = (fNamne,lName) =>  fNamne+lName;
+
+
+console.log(fullName('Arlif','Tagreee'));
