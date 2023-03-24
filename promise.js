083a145180df080bf8a8fa5ce38bf91a97970ed2
ArlifTagaree <liffism9@gmail.com>
2023-03-24 20:43:28 +0700

    Promise

---
+++ promise.js
@@ -0,0 +1,24 @@
+const prom = (params) => {
+    return new Promise((resolve, reject) => {
+      if(params === true) {
+        resolve('Hello World')
+      } else {
+        reject('Error')
+      }
+    })
+  }
+  
+  const handleSuccess = (data) => {
+    console.log(data)
+  }
+  
+  const handleFailure = (data) => {
+    console.log('this is fail')
+    console.log(data)
+  }
+  
+  // then รับ params 2 ตัว คือ
+  // 1 รับ resolve
+  // 2 รับ reject
+  prom(false).then(handleSuccess, handleFailure)
+  
